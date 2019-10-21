73a1ef4acfb05b18d4d1b783c20e74a85f553f2b
shrijan <shrijan.siristechnology@gmail.com>
2019-10-21 10:29:23 +0545

    nep-98-pull-n-articles: graphql test call file added

---
+++ src/database/graphqlTestCall.js
@@ -0,0 +1,27 @@
+const { graphql } = require('graphql')
+const { importSchema } = require('graphql-import')
+const { makeExecutableSchema } = require('graphql-tools')
+
+const { resolver } = require('./resolvers')
+const typeDefs = importSchema('./typeDefs.graphql')
+
+const schema = makeExecutableSchema({ typeDefs, resolver })
+
+const graphqlTestCall = async (query, variables, userId) => {
+	return graphql(
+		schema,
+		query,
+		undefined,
+		{
+			req: {},
+			res: {
+				clearCookie: () => {},
+			},
+		},
+		variables,
+	)
+}
+
+module.exports = {
+	graphqlTestCall,
+}
