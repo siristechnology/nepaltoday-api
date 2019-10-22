1f6832d45a8b0c60d3ce7bfd02777f6106767146
shrijan <shrijan.siristechnology@gmail.com>
2019-10-22 15:00:11 +0545

    nep-98-pull-n-articles: getarticles test can not be completed

--- src/database/resolvers.getArticles.test.js
+++ src/database/resolvers.getArticles.test.js
@@ -3,9 +3,18 @@ const {
 		Query: { getArticles },
 	},
 } = require('./resolvers')
+const { importSchema } = require('graphql-import')
+const typeDefs = importSchema('src/database/typeDefs.graphql') /* Warning: Must be an absolute path */
+
+// const ArticleMock = require('./mocks')
+
+console.log('_______________article_______________', typeDefs)
 
 describe('Resolvers Query getArticles', () => {
 	it('should call Article.find for each category', () => {
-		console.log('Test getArticles here', getArticles)
+		// const articles = await getArticles(ArticleMock)
+		// expect(articles.length).toBeGreaterThan(0)
+
+		console.log(getArticles)
 	})
 })
