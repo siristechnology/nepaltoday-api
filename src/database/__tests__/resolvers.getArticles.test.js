43f85a77d4d10e5c88abf29f0e5c449a484b69da
shrijan <shrijan.siristechnology@gmail.com>
2019-10-23 21:23:41 +0545

    nep-98-pull-n-articles: removed unnecessary codes

--- src/database/__tests__/resolvers.getArticles.test.js
+++ src/database/__tests__/resolvers.getArticles.test.js
@@ -3,25 +3,11 @@ const {
 		Query: { getArticles },
 	},
 } = require('../resolvers')
-// const mongoose = require('mongoose')
-// const Mockgoose = require('mock-mongoose').Mockgoose
-// const mockgoose = new Mockgoose(mongoose)
 import mockingoose from 'mockingoose'
-
-// const { importSchema } = require('graphql-import')
-// const typeDefs = importSchema('src/database/typeDefs.graphql') /* Warning: Must be an absolute path */
 
 import { GetMockArticle } from '../mocks'
 
 describe('Resolvers Query getArticles', () => {
-	// beforeAll(function(done) {
-	// 	mockgoose.prepareStorage().then(function() {
-	// 		mongoose.connect('mongodb://example.com/TestingDB', function(err) {
-	// 			done(err)
-	// 		})
-	// 	})
-	// })
-
 	it('should call Article.find for each category', async () => {
 		const { mongooseSchema } = require('nepaltoday-db-service')
 		mongooseSchema.Article.schema.path('source', Object)
