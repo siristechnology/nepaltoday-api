c2b5585236d99ae9ed6119acb865aca45c69ca02
shrijan <shrijan.siristechnology@gmail.com>
2019-10-24 22:51:14 +0545

    nep-129-create-mutation-api:  timezone is added, db is used from new tag, removed console

--- src/database/resolvers.js
+++ src/database/resolvers.js
@@ -1,71 +1,66 @@
-const _ = require("lodash");
-const { mongooseSchema } = require("nepaltoday-db-service");
+const _ = require('lodash')
+const { mongooseSchema } = require('nepaltoday-db-service')
 
-const { User } = mongooseSchema;
-const { categories } = require("../config/category");
-const { getSortedArticle } = require("../helper/articleHelper");
+const { User } = mongooseSchema
+const { categories } = require('../config/category')
+const { getSortedArticle } = require('../helper/articleHelper')
 
 exports.resolver = {
-  Query: {
-    getArticles: async (parent, args, { Article }) => {
-      args.criteria = args.criteria || {};
-      args.criteria.lastQueryDate =
-        args.criteria.lastQueryDate || new Date("2001-01-01");
-      args.criteria.lastArticleId =
-        args.criteria.lastArticleId || "000000000000000000000000";
+	Query: {
+		getArticles: async (parent, args, { Article }) => {
+			args.criteria = args.criteria || {}
+			args.criteria.lastQueryDate = args.criteria.lastQueryDate || new Date('2001-01-01')
+			args.criteria.lastArticleId = args.criteria.lastArticleId || '000000000000000000000000'
 
-      const promises = categories.map(async category => {
-        const _articles = await Article.find({
-          category,
-          link: { $ne: null },
-          modifiedDate: { $gt: new Date(args.criteria.lastQueryDate) },
-          _id: { $gt: args.criteria.lastArticleId }
-        })
-          .lean()
-          .populate("source")
-          .sort({ _id: -1 })
-          .limit(20);
+			const promises = categories.map(async category => {
+				const _articles = await Article.find({
+					category,
+					link: { $ne: null },
+					modifiedDate: { $gt: new Date(args.criteria.lastQueryDate) },
+					_id: { $gt: args.criteria.lastArticleId },
+				})
+					.lean()
+					.populate('source')
+					.sort({ _id: -1 })
+					.limit(20)
 
-        return [..._articles];
-      });
+				return [..._articles]
+			})
 
-      // console.log('Printing articles before sorting', articles.map(a => a.title))
+			// console.log('Printing articles before sorting', articles.map(a => a.title))
 
-      const articles = await Promise.all(promises);
+			const articles = await Promise.all(promises)
 
-      const articleFlatterend = _.flatten(articles);
-      const sortedArticles = await getSortedArticle(articleFlatterend);
+			const articleFlatterend = _.flatten(articles)
+			const sortedArticles = await getSortedArticle(articleFlatterend)
 
-      return sortedArticles;
-    },
+			return sortedArticles
+		},
 
-    getTweets: async (parent, args, { Tweet }) => {
-      args.criteria = args.criteria || {};
-      args.criteria.lastQueryDate =
-        args.criteria.lastQueryDate || new Date("2001-01-01");
-      args.criteria.lastTweetId =
-        args.criteria.lastTweetId || "000000000000000000000000";
+		getTweets: async (parent, args, { Tweet }) => {
+			args.criteria = args.criteria || {}
+			args.criteria.lastQueryDate = args.criteria.lastQueryDate || new Date('2001-01-01')
+			args.criteria.lastTweetId = args.criteria.lastTweetId || '000000000000000000000000'
 
-      const tweets = await Tweet.find()
-        .populate("twitterHandle")
-        .sort({ publishedDate: -1 })
-        .limit(100);
+			const tweets = await Tweet.find()
+				.populate('twitterHandle')
+				.sort({ publishedDate: -1 })
+				.limit(100)
 
-      return tweets;
-    }
-  },
-  Mutation: {
-    storeFcmToken: async (parent, args) => {
-      const {
-        input: { fcmToken, countryCode }
-      } = args;
-      console.log("_______________fcmtoken_______________", fcmToken);
-      console.log("_______________countrycode_______________", countryCode);
-      const user = await User.create({
-        fcmToken,
-        countryCode
-      });
-      return user;
-    }
-  }
-};
+			return tweets
+		},
+	},
+	Mutation: {
+		storeFcmToken: async (parent, args) => {
+			const {
+				input: { fcmToken, countryCode, timeZone },
+			} = args
+			const user = await User.create({
+				fcmToken,
+				countryCode,
+				timeZone,
+			})
+			return user
+		},
+	},
+}
