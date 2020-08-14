3c4cca6bc47a9872731261e448aa5fbaa999f4fe
Shiva Aryal <shivaaryalj7@gmail.com>
2020-08-14 11:29:52 +0545

    Page token moved to env

--- src/jobs/facebookPost/index.js
+++ src/jobs/facebookPost/index.js
@@ -1,7 +1,6 @@
 const axios = require('axios')
 const { newsDbService } = require('../../db-service')
 const { verifyFacebookPostTime } = require('./facebookPostHelper')
-const page_id = 102217588264766
 const { FacebookLongLiveToken } = require('./../../db-service/database/mongooseSchema')
 require('dotenv').config()
 
@@ -12,11 +11,11 @@ module.exports = async function(){
         if(verifyPostEligibleTime){   
             const facebookLongLiveTokens = await FacebookLongLiveToken.findOne({}, {}, { sort: { createdDate: -1 } }).lean()
             if(facebookLongLiveTokens){
-                const pageTokens = await axios.get(encodeURI(`https://graph.facebook.com/${page_id}?fields=access_token&access_token=${facebookLongLiveTokens.longLiveToken}`))
+                const pageTokens = await axios.get(encodeURI(`https://graph.facebook.com/${process.env.FACEBOOK_PAGE_ID}?fields=access_token&access_token=${facebookLongLiveTokens.longLiveToken}`))
                 const latestArticle = await newsDbService.getLatestNewsArticle()
                 let articleTitle = latestArticle[0].title
                 let articleLink = latestArticle[0].link
-                await axios.post(encodeURI(`https://graph.facebook.com/${page_id}/feed?message=${articleTitle}&link=${articleLink}&access_token=${pageTokens.data.access_token}`))
+                await axios.post(encodeURI(`https://graph.facebook.com/${process.env.FACEBOOK_PAGE_ID}/feed?message=${articleTitle}&link=${articleLink}&access_token=${pageTokens.data.access_token}`))
                 console.log("posted to faceboook")
             
                 // updating token
