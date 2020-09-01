98c97b2fd1a60f593a0dc2b8c0c1e80a838e00cc
Suraj Shrestha <syuraj@gmail.com>
2020-08-31 21:52:51 -0400

    minor refactor

--- src/jobs/NewsCrawlerTrigger/__tests__/news-crawler.test.js
+++ src/jobs/NewsCrawlerTrigger/__tests__/news-crawler.test.js
@@ -1,15 +1,10 @@
 const NewsCrawler = require('news-crawler')
-const { dbConnection } = require('../../../helper/connectionHelper')
 const SourceConfig = require('../../../config/news-source-config.json')
 
 jest.setTimeout(120000)
 
-beforeAll(async () => {
-	await dbConnection()
-})
-
-describe('NewsCrawlerTrigger integration', () => {
-	it('Integration test', async () => {
+describe('news-crawler', () => {
+	it('library test', async () => {
 		const articles = await NewsCrawler(SourceConfig.slice(0, 1), 1)
 		console.log('printing articles', articles)
 		expect(articles.length).toBeGreaterThan(0)
