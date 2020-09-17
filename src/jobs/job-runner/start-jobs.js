7c7f4205c87563302ccdfc36a49e0516f2a4b7d4
Shiva Aryal <shivaaryalj7@gmail.com>
2020-09-17 15:35:03 +0545

    NEP-241 : Source selector corrected

--- src/jobs/job-runner/start-jobs.js
+++ src/jobs/job-runner/start-jobs.js
@@ -21,49 +21,49 @@ module.exports = async function () {
 		crawler()
 	})
 
-	// agenda.define('notify users', async (job) => {
-	// 	logger.info('notify users job started')
-	// 	notifier(console)
-	// })
+	agenda.define('notify users', async (job) => {
+		logger.info('notify users job started')
+		notifier(console)
+	})
 
-	// agenda.define('pull tweets', async (job) => {
-	// 	logger.info('pull tweets job started')
-	// 	twitterJob()
-	// })
+	agenda.define('pull tweets', async (job) => {
+		logger.info('pull tweets job started')
+		twitterJob()
+	})
 
-	// agenda.define('fetch corona stats', async (job) => {
-	// 	logger.info('corona job started')
-	// 	coronaJob()
-	// })
+	agenda.define('fetch corona stats', async (job) => {
+		logger.info('corona job started')
+		coronaJob()
+	})
 
-	// agenda.define('fetch district corona stats', async (job) => {
-	// 	logger.info('fetch district corona job started')
-	// 	districtCoronaJob()
-	// })
+	agenda.define('fetch district corona stats', async (job) => {
+		logger.info('fetch district corona job started')
+		districtCoronaJob()
+	})
 
-	// agenda.define('fetch trending', async (job) => {
-	// 	logger.info('fetch trending job started')
-	// 	trendingJob()
-	// })
+	agenda.define('fetch trending', async (job) => {
+		logger.info('fetch trending job started')
+		trendingJob()
+	})
 
-	// agenda.define('post to facebook', async (job) => {
-	// 	logger.info('posting to facebook started')
-	// 	facebookPost(console)
-	// })
+	agenda.define('post to facebook', async (job) => {
+		logger.info('posting to facebook started')
+		facebookPost(console)
+	})
 
-	// agenda.define('fetch trending topics', async (job) => {
-	// 	logger.info('fetch trending topics started')
-	// 	trendingTopic()
-	// })
+	agenda.define('fetch trending topics', async (job) => {
+		logger.info('fetch trending topics started')
+		trendingTopic()
+	})
 
 	await agenda.start()
 
-	await agenda.every('2 minutes', 'crawl articles')
-	// await agenda.every('5 minutes', 'notify users')
-	// await agenda.every('10 minutes', 'pull tweets')
-	// await agenda.every('2 hours', 'fetch corona stats')
-	// await agenda.every('2 hours', 'fetch district corona stats')
-	// await agenda.every('1 hours', 'fetch trending')
-	// await agenda.every('136 minutes', 'post to facebook')
-	// await agenda.every('24 hours','fetch trending topics')
+	await agenda.every('30 minutes', 'crawl articles')
+	await agenda.every('5 minutes', 'notify users')
+	await agenda.every('10 minutes', 'pull tweets')
+	await agenda.every('2 hours', 'fetch corona stats')
+	await agenda.every('2 hours', 'fetch district corona stats')
+	await agenda.every('1 hours', 'fetch trending')
+	await agenda.every('136 minutes', 'post to facebook')
+	await agenda.every('24 hours','fetch trending topics')
 }
