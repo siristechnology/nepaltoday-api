e06e5678ec728cbcf442b3c6769c4a5dc5411279
Shiva Aryal <shivaaryalj7@gmail.com>
2020-12-28 10:23:25 +0545

    NEP-314-fetching world summary by adding pomber countrty stats

--- src/jobs/job-runner/start-jobs.js
+++ src/jobs/job-runner/start-jobs.js
@@ -17,60 +17,60 @@ module.exports = async function () {
 
 	const agenda = new Agenda({ db: { address: process.env.DATABASE_URL } })
 
-	agenda.define('crawl articles', async (job) => {
-		logger.info('crawl articles job started')
-		crawler()
-	})
+	// agenda.define('crawl articles', async (job) => {
+	// 	logger.info('crawl articles job started')
+	// 	crawler()
+	// })
 
-	agenda.define('notify users', async (job) => {
-		logger.info('notify users job started')
-		notifier()
-	})
+	// agenda.define('notify users', async (job) => {
+	// 	logger.info('notify users job started')
+	// 	notifier()
+	// })
 
-	agenda.define('pull tweets', async (job) => {
-		logger.info('pull tweets job started')
-		twitterJob()
-	})
+	// agenda.define('pull tweets', async (job) => {
+	// 	logger.info('pull tweets job started')
+	// 	twitterJob()
+	// })
 
 	agenda.define('fetch corona stats', async (job) => {
 		logger.info('corona job started')
 		coronaJob()
 	})
 
-	agenda.define('fetch district corona stats', async (job) => {
-		logger.info('fetch district corona job started')
-		districtCoronaJob()
-	})
+	// agenda.define('fetch district corona stats', async (job) => {
+	// 	logger.info('fetch district corona job started')
+	// 	districtCoronaJob()
+	// })
 
-	agenda.define('fetch trending', async (job) => {
-		logger.info('fetch trending job started')
-		trendingJob()
-	})
+	// agenda.define('fetch trending', async (job) => {
+	// 	logger.info('fetch trending job started')
+	// 	trendingJob()
+	// })
 
-	agenda.define('fetch trending topics', async (job) => {
-		logger.info('fetch trending topics started')
-		trendingTopic()
-	})
+	// agenda.define('fetch trending topics', async (job) => {
+	// 	logger.info('fetch trending topics started')
+	// 	trendingTopic()
+	// })
 
-	agenda.define('check news from sources', async (job) => {
-		logger.info('checking news job started')
-		newsChecker()
-	})
+	// agenda.define('check news from sources', async (job) => {
+	// 	logger.info('checking news job started')
+	// 	newsChecker()
+	// })
 
-	agenda.define('fetch trending tags', async (job) => {
-		logger.info('fetch trending tags started')
-		trendingTags()
-	})
+	// agenda.define('fetch trending tags', async (job) => {
+	// 	logger.info('fetch trending tags started')
+	// 	trendingTags()
+	// })
 
 	await agenda.start()
 
-	await agenda.every('30 minutes', 'crawl articles')
-	await agenda.every('5 minutes', 'notify users')
-	await agenda.every('20 minutes', 'pull tweets')
-	await agenda.every('1 hours', 'fetch corona stats')
-	await agenda.every('20 minutes', 'fetch district corona stats')
-	await agenda.every('2 hours', 'fetch trending')
-	await agenda.every('24 hours', 'fetch trending topics')
-	await agenda.every('24 hours', 'check news from sources')
-	await agenda.every('5 hours', 'fetch trending tags')
+	// await agenda.every('30 minutes', 'crawl articles')
+	// await agenda.every('5 minutes', 'notify users')
+	// await agenda.every('20 minutes', 'pull tweets')
+	await agenda.every('5 minutes', 'fetch corona stats')
+	// await agenda.every('20 minutes', 'fetch district corona stats')
+	// await agenda.every('2 hours', 'fetch trending')
+	// await agenda.every('24 hours', 'fetch trending topics')
+	// await agenda.every('24 hours', 'check news from sources')
+	// await agenda.every('5 hours', 'fetch trending tags')
 }
