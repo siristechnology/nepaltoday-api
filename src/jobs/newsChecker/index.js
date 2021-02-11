2c366dcedf5fabb0bf112d6685ff268e9a353968
Shiva Aryal <shivaaryalj7@gmail.com>
2021-02-11 07:56:46 +0545

    NEP-354 : mailgun integrated for nodemailer

--- src/jobs/newsChecker/index.js
+++ src/jobs/newsChecker/index.js
@@ -5,17 +5,16 @@ const logger = require("../../config/logger")
 
 module.exports = async function(){
     try{
-        // const lastDay = new Date((new Date()).getTime() - 24 * 60 * 60 * 1000)
-        // for(const source of getAllSources()){
-        //     const todayArticles = await Article.find({
-        //         sourceName: source.sourceName,
-        //         createdAt: { $gte: lastDay }
-        //     })
-        //     if(todayArticles.length==0){
-        //         sendMail(source.sourceName)
-        //     }       
-        // }
-        sendMail('Test')
+        const lastDay = new Date((new Date()).getTime() - 24 * 60 * 60 * 1000)
+        for(const source of getAllSources()){
+            const todayArticles = await Article.find({
+                sourceName: source.sourceName,
+                createdAt: { $gte: lastDay }
+            })
+            if(todayArticles.length==0){
+                sendMail(source.sourceName)
+            }       
+        }
     }catch(error){
         logger.info("News checker error ", error)
     }
@@ -24,37 +23,19 @@ module.exports = async function(){
 
 const sendMail = async (sourceName) => {
     try{
-        // const transporter = nodeMailer.createTransport({
-        //     service:'gmail',
-        //     auth:{
-        //         user: process.env.NOTIFY_EMAIL_ID,
-        //         pass:process.env.NOTIFY_EMAIL_PASSWORD
-        //     }
-        // })
-
-        // const mailOptions = {
-        //     from: process.env.NOTIFY_EMAIL_ID,
-        //     to: process.env.RECEIVER_MAILS,
-        //     subject: 'Articles not being fetched from library through '+sourceName,
-        //     text: 'Articles not being fetched from library through '+sourceName
-        // }
-
-        // await transporter.sendMail(mailOptions)
-
-        const mailgunApiKey = '08683f5bcab9cbc75ca241cd5279ba64-4de08e90-1974be39'
-        const domain = 'sandboxb192aca120594916840c0ccd5cb144cd.mailgun.org'
+        const mailgunApiKey = process.env.MAILGUN_API_KEY
+        const domain = process.env.MAILGUN_DOMAIN
         const from = 'shiva.siristechnology@gmail.com'
 
         const mailgun = new Mailgun({apiKey: mailgunApiKey, domain: domain});
 
         const data = {
             from,
-            to: 'shivaaryalj7@gmail.com',
-            subject:  'Articles not being fetched from library through ',
-            text:  'Articles not being fetched from library through '
+            to: process.env.RECEIVER_MAILS,
+            subject:  'Articles not being fetched from library through '+sourceName,
+            text:  'Articles not being fetched from library through '+sourceName
         }
         await mailgun.messages().send(data)
-
 
     }catch(error){
         logger.info("Error while sending mail ",error)
